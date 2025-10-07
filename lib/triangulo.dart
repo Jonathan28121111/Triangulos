bd93e6e0c7fd19a66d541d76495593f84369817d
Jonathan28121111 <jonathan120428@gmail.com>
2025-10-07 14:27:37 -0700

    agrefamos el void validar para que no haya lados con medidas incorrectas

--- lib/triangulo.dart
+++ lib/triangulo.dart
@@ -17,5 +17,19 @@ class Triangulo {
   final int lado2;
   final int lado3;
   
+  Triangulo(this.lado1, this.lado2, this.lado3) {
+    _validar();
+  }
+  
+  void _validar() {
+    if (lado1 <= 0 || lado2 <= 0 || lado3 <= 0) {
+      throw TrianguloException('Los lados deben ser mayores que cero');
+    }
+    
+    if (lado1 + lado2 <= lado3 || 
+        lado1 + lado3 <= lado2 || 
+        lado2 + lado3 <= lado1) {
+      throw TrianguloException('No cumple la desigualdad triangular');
+    }
 
   }
