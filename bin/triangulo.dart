d54b0f505a4422e5f9a0197a757504c2cba017de
Jonathan28121111 <jonathan120428@gmail.com>
2025-10-07 14:22:50 -0700

    Creacion del proyecto

---
+++ bin/triangulo.dart
@@ -0,0 +1,5 @@
+import 'package:triangulo/triangulo.dart' as triangulo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${triangulo.calculate()}!');
+}
---
+++ test/triangulo_test.dart
@@ -0,0 +1,8 @@
+import 'package:triangulo/triangulo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
