577f83ccd7f544d269cd22b0d76e4ea9e4d7205e
Jonathan28121111 <jonathan120428@gmail.com>
2025-10-07 14:29:40 -0700

    agregamos test de triangulo equilatero

--- test/triangulo_test.dart
+++ test/triangulo_test.dart
@@ -2,4 +2,14 @@ import 'package:triangulo/triangulo.dart';
 import 'package:test/test.dart';
 
 void main() {
+  print('Ejecutando tests del Clasificador de Triangulos\n');
+  
+  int pasados = 0;
+  int total = 0;
+  
+  total++;
+  if (test('Triangulo equilatero (3,3,3)', () {
+    var t = Triangulo(3, 3, 3);
+    return t.clasificar() == TipoTriangulo.equilatero;
+  })) pasados++;
 }
